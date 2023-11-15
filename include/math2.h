d46c72ada53bae91551557d673b0fb89bae390c0
Neo <250603123@qq.com>
2023-11-15 16:04:38 +0800

    Update math2.h

--- include/math2.h
+++ include/math2.h
@@ -34,7 +34,7 @@
     #define MATH2_PI         3.14159265358979323846f
 #endif
 
-/* trigonometric function extension */
+/* 角度弧度转换(trigonometric function extension) */
 #if !defined math2_deg_to_rad
     #define math2_deg_to_rad(deg)  ((deg) * MATH2_PI / 180.0f)
 #endif
@@ -58,6 +58,7 @@ extern float acotf(float x);
 extern float asecf(float x);
 extern float acscf(float x);
 
+/* 角度归一到[-Pi, Pi] */
 extern double math2_normalize_angle_lf(double sita);
 extern float math2_normalize_angle_f(float sita);
 
@@ -67,22 +68,36 @@ typedef float (*math2_unaryfunction_fpt)(float x);
 typedef int (*math2_unaryfunction_pt)(int x);
 typedef long (*math2_unaryfunction_lpt)(long x);
 
-/* derivation */
+/* 常规函数一阶导(derivation) */
 extern double math2_derivative_lf(const math2_unaryfunction_lfpt f, double x, double dx);
 extern float math2_derivative_f(const math2_unaryfunction_fpt f, float x, float dx);
-#define math2_derivative_h(f,x)  math2_derivative_lf((f), (x), 0.000001)
-#define math2_derivative_hf(f,x) math2_derivative_f((f), (x), 0.000001f)
+#define math2_derivative_h(f,x)  math2_derivative_lf((f), (x), EPSINON)
+#define math2_derivative_hf(f,x) math2_derivative_f((f), (x), EPSINONf)
 
-/* factorial and double factorial */
+/* 阶乘和二阶阶乘(factorial and double factorial) */
 extern int math2_factorial(int n);
 extern long math2_factorial_l(long n);
 extern int math2_doublefactorial(int n);
 extern long math2_doublefactorial_l(long n);
 
-/* high step radication */
+/* 高阶开方 */
 #define math2_radication_lf(x, t) pow((x), 1.0 / (t))
 #define math2_radication_f(x, t) powf((x), 1.0f / (t))
 
-/* greatest common divisor(gcd) */
+/* 最大公约数 (GCD, greatest common divisor) */
 extern int math2_gcd(int a, int b);
 extern long math2_gcd_l(long a, long b);
+
+/* 方差 */
+extern double variance_lf(const long *origin, unsigned int count);
+extern float variance_f(const float *origin, unsigned int count);
+/* 标准差 */
+extern double stdev_lf(const long *origin, unsigned int count);
+extern float stdev_f(const float *origin, unsigned int count);
+/* 协方差 */
+extern double covariance_lf(const long *origin1, const long *origin2, unsigned int count);
+extern float covariance_f(const float *origin1, const float *origin2, unsigned int count);
+/* 均方误差(MSE, mean square error) */
+extern double mse_lf(const double *origin1, const double *origin2, unsigned int count);
+extern float mse_f(const float *origin1, const float *origin2, unsigned int count);
+extern double mse_l(const long *origin1, const long *origin2, unsigned int count);
