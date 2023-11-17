6d79642be544fde378c66ef4ef69104321e2efa3
Neo <250603123@qq.com>
2023-11-17 16:14:35 +0800

    Update math2.c

--- nshost/math2.c
+++ nshost/math2.c
@@ -1,6 +1,5 @@
 #include "math2.h"
 
-
 /* trigonometric function extension */
 double cot(double x)
 {
@@ -62,7 +61,7 @@ float acscf(float x)
     return asinf(1.0f / x);
 }
 
-double math2_normalize_angle_lf(double sita)
+double normalize_angle_lf(double sita)
 {
     int tp;
 	double angle;
@@ -87,7 +86,7 @@ double math2_normalize_angle_lf(double sita)
 	return angle;
 }
 
-float math2_normalize_angle_f(float sita)
+float normalize_angle_f(float sita)
 {
     int tp;
     float angle;
@@ -113,36 +112,88 @@ float math2_normalize_angle_f(float sita)
 }
 
 /* derivation */
-double math2_derivative_lf(const math2_unaryfunction_lfpt f, double x, double dx)
+double derivative_lf(const math2_unaryfunction_lfpt f, double x, double dx)
+{
+    return (f(x + dx) - f(x)) / dx;
+}
+float derivative_f(const math2_unaryfunction_fpt f, float x, float dx)
 {
     return (f(x + dx) - f(x)) / dx;
 }
 
-float math2_derivative_f(const math2_unaryfunction_fpt f, float x, float dx)
+int differentiable_lf(const double *samples, const uint64_t *timestamps, unsigned int n, double limit, double *gap)
 {
-    return (f(x + dx) - f(x)) / dx;
+    double left_derivation, right_derivation, dt, diff;
+    unsigned int i;
+
+    if (n < 3) {
+        return 0;
+    }
+
+    for (i = 1; i < n - 1; i++) {
+        dt = (double)((double)timestamps[i] - timestamps[i - 1]) / 1000000.0;
+        left_derivation = (samples[i] - samples[i - 1]) / dt;
+        right_derivation = (samples[i + 1] - samples[i]) / dt;
+
+        // check differentiable
+        diff = fabs(left_derivation - right_derivation);
+        if (is_float_gt(diff, limit) ) {
+            if (gap) {
+                *gap = diff;
+            }
+            return i;
+        }
+    }
+    return 0;
+}
+
+MATH2_BOOL is_sequence_differentiable_f(const float samples[3], const uint64_t *timestamps, unsigned int n, float limit, float *gap)
+{
+    float left_derivation, right_derivation, diff;
+    double dt;
+    unsigned int i;
+
+    if (n < 3) {
+        return 0;
+    }
+
+    for (i = 1; i < n - 1; i++) {
+        dt = (double)((double)timestamps[i] - timestamps[i - 1]) / 1000000.0;
+        left_derivation = (samples[i] - samples[i - 1]) / dt;
+        right_derivation = (samples[i + 1] - samples[i]) / dt;
+
+        // check differentiable
+        diff = fabsf(left_derivation - right_derivation);
+        if (is_float_gt(diff, limit) ) {
+            if (gap) {
+                *gap = diff;
+            }
+            return i;
+        }
+    }
+    return 0;
 }
 
 /* factorial */
-int math2_factorial(int n)
+int factorial(int n)
 {
-    return n > 1 ? n * math2_factorial(n - 1) : 1;
+    return n > 1 ? n * factorial(n - 1) : 1;
 }
-long math2_factorial_l(long n)
+long factorial_l(long n)
 {
-    return n > 1 ? n * math2_factorial_l(n - 1) : 1;
+    return n > 1 ? n * factorial_l(n - 1) : 1;
 }
-int math2_doublefactorial(int n)
+int doublefactorial(int n)
 {
-    return n > 1 ? n * math2_doublefactorial(n - 2) : 1;
+    return n > 1 ? n * doublefactorial(n - 2) : 1;
 }
-long math2_doublefactorial_l(long n)
+long doublefactorial_l(long n)
 {
-    return n > 1 ? n * math2_doublefactorial_l(n - 2) : 1;
+    return n > 1 ? n * doublefactorial_l(n - 2) : 1;
 }
 
 /* greatest common divisor(gcd) */
-int math2_gcd(int a, int b)
+int gcd(int a, int b)
 {
     int r;
 
@@ -159,7 +210,7 @@ int math2_gcd(int a, int b)
     return a;
 }
 
-long math2_gcd_l(long a, long b)
+long gcd_l(long a, long b)
 {
     long r;
 
@@ -177,6 +228,40 @@ long math2_gcd_l(long a, long b)
 }
 
 /* statistical mathematics */
+double average_lf(const double *origin, unsigned int count)
+{
+    unsigned int i;
+    double sum;
+
+    if (!origin || 0 == count) {
+        return 0.0;
+    }
+
+    sum = 0.0;
+    for (i = 0; i < count; i++) {
+        sum += origin[i];
+    }
+
+    return sum / count;
+}
+
+float average_f(const float *origin, unsigned int count)
+{
+    unsigned int i;
+    float sum;
+
+    if (!origin || 0 == count) {
+        return 0.0f;
+    }
+
+    sum = 0.0f;
+    for (i = 0; i < count; i++) {
+        sum += origin[i];
+    }
+
+    return sum / count;
+}
+
 double variance_lf(const long *origin, unsigned int count)
 {
     unsigned int i;
@@ -334,7 +419,7 @@ float mse_f(const float *origin1, const float *origin2, unsigned int count)
 double mse_l(const long *origin1, const long *origin2, unsigned int count)
 {
     unsigned int i;
-    float sum;
+    long sum;
 
     if (!origin1 || !origin2 || 0 == count) {
         return 0.0;
@@ -345,5 +430,53 @@ double mse_l(const long *origin1, const long *origin2, unsigned int count)
         sum += (origin1[i] - origin2[i]) * (origin1[i] - origin2[i]);
     }
 
-    return sum / count;
+    return (double)sum / count;
+}
+
+double uniform_random_lf(double a, double b, long *seed)
+{
+	double t;
+
+	*seed = 2045.0 * (*seed) + 1;
+	*seed = *seed - (*seed / 1048576) * 1048576;
+	t = (*seed) / 1048576.0;
+	t = a + (b - a) * t;
+	return t;
+}
+
+float uniform_random_f(float a, float b, long *seed)
+{
+	float t;
+
+	*seed = 2045.0 * (*seed) + 1;
+	*seed = *seed - (*seed / 1048576) * 1048576;
+	t = (*seed) / 1048576.0;
+	t = a + (b - a) * t;
+	return t;
+}
+
+double gaussian_random_lf(double mean, double sigma, long *seed)
+{
+	int i;
+	double x, y;
+
+	for (x=0, i = 0; i < 12; i++) {
+		x += uniform_random_lf(0.0, 1.0, seed);
+	}
+	x = x - 6;
+	y = mean + x * sigma;
+	return y;
+}
+
+float gaussian_random_f(float mean, float sigma, long *seed)
+{
+	int i;
+	float x, y;
+
+	for (x=0, i = 0; i < 12; i++) {
+		x += uniform_random_f(0.0, 1.0, seed);
+	}
+	x = x - 6;
+	y = mean + x * sigma;
+	return y;
 }
